efa509bad9c4dd6d965b98bb80c122ef8004577d
coderift <normansoft31@gmail.com>
2018-01-01 12:00:00 +0200

    commit for 2018-01-01 by coderift

--- bash.sh
+++ bash.sh
@@ -2,9 +2,8 @@ start_date="2018-01-01"
 end_date="2024-12-01"
 auth="coderift"
 
-while [ "$start_date" != "$end_date" ]; do
-    echo "Commit on $start_date"
-    git add .
+git add .
+while [ "$start_date" != "$end_date" ]; do    
     GIT_COMMITTER_DATE="$start_date 12:00:00" GIT_AUTHOR_DATE="$start_date 12:00:00" git commit -m "commit for $start_date by $auth"
     start_date=$(date -I -d "$start_date + 1 day")
 done
