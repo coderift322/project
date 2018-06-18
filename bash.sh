e048b8e5036d4668858ea084f013503d45dacf96
coderift <normansoft31@gmail.com>
2018-06-18 12:00:00 +0300

    commit for 2018-06-18 by coderift

--- bash.sh
+++ bash.sh
@@ -3,7 +3,7 @@ end_date="2024-12-01"
 auth="coderift"
 
 while [ "$start_date" != "$end_date" ]; do
-    echo "Commit on $start_date" >> fakefile.txt
+    echo "Commit on $start_date"
     git add .
     GIT_COMMITTER_DATE="$start_date 12:00:00" GIT_AUTHOR_DATE="$start_date 12:00:00" git commit -m "commit for $start_date by $auth"
     start_date=$(date -I -d "$start_date + 1 day")
